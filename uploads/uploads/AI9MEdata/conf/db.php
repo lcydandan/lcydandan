99b6e8898f19fdcbba0929c8fcf83e53862e58cc
lcydandan <121413438@qq.com>
2013-12-10 19:14:57 +0800

    源码上传

---
+++ uploads/uploads/AI9MEdata/conf/db.php
@@ -0,0 +1,11 @@
+<?php
+return array (
+  'DB_TYPE' => 'mysql',
+  'DB_HOST' => 'localhost',
+  'DB_PORT' => '3306',
+  'DB_NAME' => 'web_test_github',
+  'DB_USER' => 'root',
+  'DB_PWD' => '',
+  'DB_PREFIX' => 'ai9me_',
+);
+?>
