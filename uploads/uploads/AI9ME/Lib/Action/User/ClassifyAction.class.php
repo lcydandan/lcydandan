3c0e3d495c956eb61b9d7a87a072247633292089
lcydandan <121413438@qq.com>
2013-12-21 23:20:45 +0800

    分类的修改的实现

--- uploads/uploads/AI9ME/Lib/Action/User/ClassifyAction.class.php
+++ uploads/uploads/AI9ME/Lib/Action/User/ClassifyAction.class.php
@@ -8,9 +8,24 @@ class ClassifyAction extends UserAction{
 		$where['token']=session('token');
 		$count=$db->where($where)->count();
 		$page=new Page($count,25);
-		$info=$db->where($where)->order('sorts desc')->limit($page->firstRow.','.$page->listRows)->select();
+		$weburl = $db->where($where)->distinct(true)->field('weburl,name')->limit($page->firstRow.','.$page->listRows)->select();
+// 		$webinfo = array();
+// 		$i = 0;
+// 		foreach ($weburl as $currenturl)
+// 		{
+// 			$condition['token'] = $where['token'];		
+// 			$condition['weburl'] = $currenturl;
+// 			$info = $db->where($condtion)->order('sorts desc')->limit($page->firstRow.','.$page->listRows)->select();
+// 			$data['url'] = $currenturl;
+// 			$data['name'] = $info['name'];
+// 			$webinfo[$i] = $data;	
+// 			$i++;		
+// 		}
+		//$info=$db->where($where)->order('sorts desc')->limit($page->firstRow.','.$page->listRows)->select();
+				
 		$this->assign('page',$page->show());
-		$this->assign('info',$info);
+		$this->assign('info',$weburl);
+		
 		$this->display();
 	}
 	
@@ -25,9 +40,16 @@ class ClassifyAction extends UserAction{
 		$this->display();
 	}
 	
+	public function editNew(){
+		$weburl['weburl'] =$this->_get('weburl');
+		
+		$info=M('Classify')->where($weburl)->order('sorts')->select();
+		$this->assign('info',$info);
+		$this->display();
+	}
+	
 	public function del(){
-		$where['id']=$this->_get('id','intval');
-		$where['uid']=session('uid');
+		$where['weburl']=$this->_get('weburl');
 		if(D(MODULE_NAME)->where($where)->delete()){
 			$this->success('操作成功',U(MODULE_NAME.'/index'));
 		}else{
@@ -59,8 +81,10 @@ class ClassifyAction extends UserAction{
 	public function insertMany()
 	{		
 		$token = session('token');
+		$db = M(MODULE_NAME);
 		$id = null;
 		$name = $this->_post('name');
+		$tpl = $this->_post('webmodel');
 		for ($i=0; $i<20; $i++)
 		{
 			$data['name'] = $name;
@@ -77,7 +101,9 @@ class ClassifyAction extends UserAction{
 			$data['status'] = '1';
 			$data['sorts'] = strval($i);
 			$data['token'] = $token;
-			$id = M(MODULE_NAME)->add($data);
+			$data['weburl'] = 'http://baidu'.date('YmdHis').'.com' ;
+			$data['tpltypename'] = $tpl;
+			$id = $db->add($data);
 			if ($id == null)
 			{
 				break;
@@ -91,13 +117,57 @@ class ClassifyAction extends UserAction{
 		{
 			$this->error('操作失败',U(MODULE_NAME.'/index'));
 		}
+	}
+	
+	public function deleteMany()
+	{
+		$token = session('token');
+	}
+	
+	public function upsaveMany()
+	{
+		$token = session('token');
+		$weburl = $this->_post('weburl');
+		$db = M(MODULE_NAME);
+		//先删除原来的
+		$where['weburl']=$this->_post('weburl');
+		$db->where($where)->delete();
 		
-		//加入3g_website数据库
-		$db = M('3g_website');
-		$webdata['name'] = $name;
-		$webdata['url'] = 'http://www.baidu.com';
-		$webdata['token'] = $token;
-		$db->add($data);
+		$id = null;
+		$name = $this->_post('name');
+		$tpl = $this->_post('webmodel');
+		for ($i=0; $i<20; $i++)
+		{
+			$data['name'] = $name;
+			$img = $this->_post('img'.$i);
+			if ($img == null)
+			{
+				continue;
+			}
+			$id = null;
+			$data['img'] = $img;
+			$data['url'] = str_replace('&amp;', "&", $this->_post('url'.$i));
+			$data['info'] = $this->_post('info'.$i);
+			$data['name'] = $this->_post('name');
+			$data['status'] = '1';
+			$data['sorts'] = strval($i);
+			$data['token'] = $token;
+			$data['weburl'] = $weburl;
+			$data['tpltypename'] = $tpl;
+			$id = $db->add($data);
+			if ($id == null)
+			{
+				break;
+			}
+		}
+		if ($id)
+		{
+			$this->success('操作成功',U(MODULE_NAME.'/index'));
+		}
+		else
+		{
+			$this->error('操作失败',U(MODULE_NAME.'/index'));
+		}
 	}
 	
 	public function upsave(){
